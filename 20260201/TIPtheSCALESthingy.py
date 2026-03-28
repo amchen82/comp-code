3e9cbdff698ad608b2eaab83c5979a1dccabb079
sanjitharnal <sanjit.m.harnal@gmail.com>
2026-03-28 15:19:11 -0400

    update

--- 20260201/TIPtheSCALESthingy.py
+++ 20260201/TIPtheSCALESthingy.py
@@ -1,7 +1,7 @@
 from pybricks.hubs import PrimeHub
 from pybricks.pupdevices import Motor
 from pybricks.robotics import DriveBase
-from pybricks.parameters import Port,Direction
+from pybricks.parameters import Port,Direction,Stop
 from pybricks.tools import multitask, run_task,wait
 
 hub = PrimeHub()
@@ -16,7 +16,7 @@ robot = DriveBase(left_motor,right_motor,wheel_diameter,axle_track)
 
 robot.use_gyro(True)
 
-robot.settings(straight_speed = 720,straight_acceleration= 400, turn_rate=600, turn_acceleration=400)
+robot.settings(straight_speed = 900,straight_acceleration= 600, turn_rate=600, turn_acceleration=400)
 hub.imu.reset_heading(0)
 
 if hub.imu.ready():
@@ -29,18 +29,34 @@ if hub.imu.ready():
     LAM.run_time(9000,1100)
     robot.straight(170)
     robot.turn(90)
-    robot.straight(-300)
-    robot.turn(48)
-    robot.straight(30)
-    RAM.run_time(850,1100) 
-    robot.straight(20)
-    robot.settings(straight_speed = 670,straight_acceleration= 400, turn_rate=600, turn_acceleration=400)
-    robot.straight(-300)
-    robot.turn(15)
-    robot.straight(80)
-    robot.turn(-40) 
-    robot.straight(-500)
+    robot.straight(-450)
+    robot.turn(50)
+    robot.straight(130)
+    RAM.run_time(850,1100)
+    robot.straight(-170)
+    RAM.run_time(-850,1100)
+    robot.straight(140)
+    robot.settings(straight_speed = 900,straight_acceleration= 600, turn_rate=600, turn_acceleration=300)
+    robot.turn(-88,Stop.NONE)
+    # robot.straight(10,Stop.NONE)
+    robot.turn(88,Stop.NONE)
+    robot.straight(-1000)
 
-    # multitask(robot.straight(-200), RAM.run_time(-2000,1400)) #dnt fix
+
+
+
+
+    # robot.turn(48)
+    # robot.straight(30)
+    # RAM.run_time(850,1100) 
+    # robot.straight(20)
+    # robot.settings(straight_speed = 670,straight_acceleration= 400, turn_rate=600, turn_acceleration=400)
+    # robot.straight(-300)
+    # robot.turn(15)
+    # robot.straight(80)
+    # robot.turn(-40) 
+    # robot.straight(-500)
+
+    # # multitask(robot.straight(-200), RAM.run_time(-2000,1400)) #dnt fix
 
 
